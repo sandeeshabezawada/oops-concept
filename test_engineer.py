5f0418eaad524023dcd6f2a7ca9966ca3e94111e
sandeeshabezawada <sandeeshaprasad@gmail.com>
2024-11-04 22:29:12 +0530

    Add files via upload

---
+++ test_engineer.py
@@ -0,0 +1,64 @@
+from Using_class.arithmetic_operations_using_class import arithmetic_op
+obj=arithmetic_op()
+res=obj.sum()
+print("sum of two numbers:",res)
+obj1=arithmetic_op()
+res=obj1.sub()
+print("subtraction:",res)
+obj2=arithmetic_op()
+res=obj2.mul()
+print("multiplication:",res)
+
+obj3=arithmetic_op()
+res=obj3.div()
+print("division:",res)
+
+obj4=arithmetic_op()
+res=obj4.mod()
+print("module:",res)
+
+obj5=arithmetic_op()
+res=obj5.even_odd()
+print("the given number is:",res)
+
+obj6=arithmetic_op()
+res=obj6.bmi_calc()
+print("BodyMassIndex:",res)
+
+obj7=arithmetic_op()
+res=obj7.fact()
+print("factorial:",res)
+
+obj8=arithmetic_op()
+res=obj8.celcius_to_foreign_heat()
+print("celcius to foreign heat:",res)
+
+obj9=arithmetic_op()
+res=obj9.discount_calc()
+print("discount is:",res)
+
+obj10=arithmetic_op()
+res=obj10.interest_calc()
+print("rate of interest is:",res)
+
+obj11=arithmetic_op()
+res=obj11.mobile_no_validation()
+print("the given mobile number is:",res)
+
+obj12=arithmetic_op()
+res=obj12.prime_no()
+print("given number is:",res)
+
+obj13=arithmetic_op()
+res=obj13.perimeter_circle()
+print("perimeter of circle is:",res)
+
+obj14=arithmetic_op()
+res=obj14.pos_negative()
+print("the given number is:",res)
+
+obj15=arithmetic_op()
+res=obj15.speed_calc()
+print("speed:",res)
+
+
